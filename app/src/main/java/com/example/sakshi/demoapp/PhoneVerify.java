1d321fde698591b1899fe1b348f6c91573fa542c
Kristen_28 <sakshichahal53@gmail.com>
2018-05-20 23:58:27 +0530

    added a new file

---
+++ app/src/main/java/com/example/sakshi/demoapp/PhoneVerify.java
@@ -0,0 +1,13 @@
+package com.example.sakshi.demoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhoneVerify extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_verify);
+    }
+}
